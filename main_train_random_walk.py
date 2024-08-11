a2f73d1cb922839bb944ddae9944024a33048c7b
ptwang773 <ptwang773@gmail.com>
2024-08-11 13:59:45 +0800

    wb: random walk with LLM (optimized)

--- main_train_random_walk.py
+++ main_train_random_walk.py
@@ -1,6 +1,6 @@
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 # encoding: utf-8
 import argparse
 import random
@@ -13,7 +13,6 @@ from data.datasets import DownStreamDataset, WalkDataset
 from utils.io_utils import load_access_street_view, load_task_data, calc, init_seed, init_logging, \
     get_graph_and_images_dual, shrink_graph
 from DualVLMWalk import ask_start_image, ask_middle_image, get_model, ask_summary_image
-import concurrent.futures
 
 embed_dims = {
     'MAE': 1024,
@@ -42,74 +41,17 @@ class Linear(nn.Module):
         return logits
 
 
-def output_words(city, index, text, model_name="minicpm"):
-    file_name = f"./log/supervised/{city}/{index}.md"
-    os.makedirs(f"./log/supervised/{city}/", exist_ok=True)
+def get_images(index, city):
+    images_path = f'/home/wangb/zhangrx/LLMInvestrigator/log/supervised/{city}/{index}_image.md'
+    with open(images_path, 'r', encoding='utf-8') as file:
+        content = file.read()
 
-    # 打开文件，追加内容，然后关闭文件
-    with open(file_name, 'a') as file:
-        file.write("\n")
-        file.write(text)
-        file.write("\n")
-
-
-def output_images(city, index, image, model_name="minicpm"):
-    file_name = f"./log/supervised/{city}/{index}_image.md"
-    os.makedirs(f"./log/supervised/{city}/", exist_ok=True)
-
-    # 打开文件，追加内容，然后关闭文件
-    with open(file_name, 'a') as file:
-        file.write("\n")
-        file.write(str(image))
-        file.write("\n")
-
-
-def random_walk(g, start_point, args, index, llm, tokenizer, images, city):
-    output_words(city, index, "# Experimental Result")
-    output_words(city, index, "##  Start Edge Caption")
-
-    id = int(g.nodes[start_point]["image"])
-    image = images[id]
-    output_words(city, index, f"![no_name](../../data/{city}/{index}/squeeze_images/{id}.jpg)")
-    output_images(city, index, id)
-
+    images = [i for i in list(content.split('\n')) if i][-10:]
     images_emb = []
-    summary = ask_start_image(llm, tokenizer, image, "")
-    output_words(city, index, summary)
-
-    for i in trange(9):
-        try:
-            neighbors = list(g.neighbors(start_point))
-            best_neighbor = -1
-            best_answer = -1
-            best_image = -1
-            for neighbor in neighbors:
-                id = int(g.nodes[neighbor]["image"])
-                now_image = images[id]
-                answer, reason = ask_middle_image(llm, tokenizer, now_image, summary)
-
-                output_words(city, index,
-                             f"![no_name](../../data/{city}/{index}/squeeze_images/{id}.jpg)")
-                output_words(city, index, reason)
-
-                if answer > best_answer:
-                    best_answer = answer
-                    best_neighbor = neighbor
-                    best_image = now_image
-                    output_words(city, index, "best answer updated!!!!!!!!!!!!!!!!")
-
-            start_point = best_neighbor
-
-            output_words(city, index, f"###  update summary")
-            summary = ask_summary_image(llm, tokenizer, best_image, summary)
-            output_words(city, index, summary)
-
-            output_images(city, index, int(g.nodes[best_neighbor]["image"]))
-        except Exception as e:
-            print(e)
-            continue
-    # exit(0)
-    return torch.tensor(images_emb).float()
+    for image in images:
+        embedding_path = f"/home/wangb/OpenVIRL/data/{city}/{index}/image_embeddings/{image}-{args.model}.npy"
+        images_emb.append(np.load(embedding_path))
+    return torch.tensor(np.array(images_emb)).float()
 
 
 def evaluate(model, loader, args, llm, tokenizer, city):
@@ -123,24 +65,15 @@ def evaluate(model, loader, args, llm, tokenizer, city):
     for index, y, s in tqdm(loader):
         index = int(index[0])
 
-        sub_g, street_views, images = get_graph_and_images_dual(index, args.city_size, args.target)
+        image_emb = get_images(index, city)
+        image_emb = torch.cat((image_emb, s), dim=0)
+        image_emb = image_emb.to(device)
+        image_latent = model(image_emb)
 
-        new_g, start_point = shrink_graph(sub_g)
-
-        random_walk(new_g, start_point, args, index, llm, tokenizer, images, city)
-        continue
-
-    return
-    #     image_emb = random_walk(new_g, start_point, args, index, llm, tokenizer, images)
-    #     image_emb = torch.cat((image_emb, s), dim=0)
-    #     image_emb = image_emb.to(device)
-    #     image_latent = model(image_emb)
-    #
-    #     all_predicts.append(float(image_latent.cpu().detach()))
-    #     all_y.append(float(y.cpu().detach()))
-    #     all_city.append(args.city_size)
-    #
-    # return calc("Eval", 233, all_predicts, all_y, all_city, None, args.city_size, args.target)
+        all_predicts.append(float(image_latent.cpu().detach()))
+        all_y.append(float(y.cpu().detach()))
+        all_city.append(args.city_size)
+    return calc("Eval", 233, all_predicts, all_y, all_city, None, args.city_size, args.target)
 
 
 city_names = ["New York City", "San Francisco", "Washington", "Chicago"]
@@ -158,6 +91,10 @@ def main(args):
     idx_dataset = []
 
     city = args.city_size
+
+    type = "sv+im"
+
+    checkpoints_dir = f"./baselines/{args.model}/checkpoints/cat-{type}-{args.city_size}-{args.target}.pt"
 
     # todo: for test, can repeat the following code with
     ava_indexs = load_access_street_view(city)
@@ -192,16 +129,14 @@ def main(args):
     # data loader
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-    # model = Linear(embed_dims[args.model], type).to(args.gpu)
-    # checkpoint = torch.load(checkpoints_dir)
-    # model.load_state_dict(checkpoint['state_dict'])
-    model = None
+    model = Linear(embed_dims[args.model], type).to(args.gpu)
+    checkpoint = torch.load(checkpoints_dir, weights_only=True)
+    model.load_state_dict(checkpoint['state_dict'])
 
     evaluate(model, test_loader, args, llm, tokenizer, city_names[city])
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -265,7 +200,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--target",
         type=int,
-        default=1,
+        default=0,
         help="Carbon or Population",
     )
 
