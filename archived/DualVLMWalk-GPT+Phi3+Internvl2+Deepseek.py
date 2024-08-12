a2acd6e7a479adbdbead9ac8db838970ba131592
Yanna <zgjsnthazy@163.com>
2024-08-12 20:04:59 +0800

    gpt

---
+++ archived/DualVLMWalk-GPT+Phi3+Internvl2+Deepseek.py
@@ -0,0 +1,393 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import random
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+from ast import literal_eval
+import re
+
+import math
+
+from tqdm import tqdm
+
+from transformers import AutoModel, AutoTokenizer
+
+import torch
+
+from utils.io_utils import load_access_street_view, get_graph_and_images_dual, output_words
+from utils.math_utils import pnt2line, get_dis
+
+import networkx as nx
+import base64
+
+import requests
+import json
+from io import BytesIO
+
+from transformers import AutoModelForCausalLM
+from transformers import AutoProcessor
+
+from lmdeploy import pipeline, TurbomindEngineConfig, ChatTemplateConfig
+
+# from deepseek_vl.models import VLChatProcessor, MultiModalityCausalLM
+# from deepseek_vl.utils.io import load_pil_images
+
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+
+city_names = ["New York City", "San Francisco", "Washington", "Chicago"]
+
+
+
+def shrink_graph(g):
+    new_g = nx.Graph()
+    for edge in g.edges(data=True):
+        node1, node2, _ = edge
+        for node in [node1, node2]:
+            color_now = g.edges[edge[0], edge[1]]["image"]
+            coord_now = g.edges[edge[0], edge[1]]["coord"]
+            for neighbor in g.neighbors(node):
+                neigh_color = g.edges[(node, neighbor)]["image"]
+                neigh_coord = g.edges[(node, neighbor)]["coord"]
+                if neigh_color != color_now:
+                    coord_now = tuple(coord_now)
+                    neigh_coord = tuple(neigh_coord)
+                    new_g.add_edge(coord_now, neigh_coord)
+                    # print(coord_now, neigh_coord)
+                    new_g.nodes[coord_now]["image"] = color_now
+                    new_g.nodes[neigh_coord]["image"] = neigh_color
+    print(len(new_g.edges))
+    return new_g, random.choice(list(new_g.nodes))
+
+
+
+def get_model(args):
+    # os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    model = AutoModel.from_pretrained('/home/wangb/zhangrx/models/MiniCPM-Llama3-V-2_5',
+                                      torch_dtype=torch.float16,
+                                      trust_remote_code=True)
+    model = model.to(device=args.gpu)
+
+    tokenizer = AutoTokenizer.from_pretrained('/home/wangb/zhangrx/models/MiniCPM-Llama3-V-2_5',
+                                              trust_remote_code=True)
+    model.eval()
+
+    return model, tokenizer
+
+
+def ask_start_image(model, tokenizer, image, previous_summary):
+    # 假设你是一个城市调查员。你在调查当地的人口密度与碳排放量。请你根据看到的图片记录一些有用的信息，这些信息会用于给其他人员进行分析。未来你还会看到一些信息的，只需要描述你看到的这张照片即可。
+    question = "Assume you are a urban investigator. " \
+               "Offer a comprehensive summary of human activity, urban infrastructure, and environments in the image"
+
+    msgs = [{'role': 'user', 'content': question}]
+
+    res = model.chat(
+        image=image,
+        msgs=msgs,
+        tokenizer=tokenizer,
+        sampling=True,
+        temperature=0.2,
+        stream=True
+    )
+
+    generated_text = ""
+    for new_text in res:
+        generated_text += new_text
+
+    print(generated_text)
+    return generated_text
+
+
+def get_score(generated_text):
+    match = re.search(r".*\{\s*\"Difference_Score\": (.+)\s*}.*", generated_text)
+
+    if match:
+        score = match.groups()[0]
+        print(score)
+        return int(score)
+    else:
+        raise ValueError("Line format is incorrect")
+
+
+def ask_summary_image(model, tokenizer, image, previous_summary):
+    # 假设你是一个城市调查员。描述你看到的图片与后文输入的summary里在human activity, urban infrastructure, and environments的区别。
+    # 最后请输出一个你估计的相似度分数，范围在0-100之间。形式为{Difference: aaa; Answer: bbb}。Summary是{previous_summary}
+    question = "Assume you are a urban investigator. " \
+               "You will input a summary of human activity, urban infrastructure, and environments in the following. " \
+               "Please modify the summary through what you see in the image. Only output the modified version" \
+               f"The summary is {previous_summary}"
+
+    msgs = [{'role': 'user', 'content': question}]
+
+    res = model.chat(
+        image=image,
+        msgs=msgs,
+        tokenizer=tokenizer,
+        sampling=True,
+        temperature=0.2,
+        stream=True
+    )
+
+    generated_text = ""
+    for new_text in res:
+        generated_text += new_text
+
+    print(generated_text)
+    return generated_text
+
+
+def ask_middle_image(model, tokenizer, image, previous_summary):
+    # 假设你是一个城市调查员。描述你看到的图片与后文输入的summary里在human activity, urban infrastructure, and environments的区别。
+    # 最后请输出一个你估计的相似度分数，范围在0-100之间。形式为{Difference: aaa; Answer: bbb}。Summary是{previous_summary}
+    question = "Assume you are a urban investigator. " \
+               "Describe the difference between the image you see and the summary following in terms of human activity, urban infrastructure, and environments. " \
+               "Please output an estimated difference score ranging from 0 to 100 in a json format as {Difference_Score: x} in the end. " \
+               f"The summary is {previous_summary}"
+
+    msgs = [{'role': 'user', 'content': question}]
+
+    while True:
+        print("Enter while")
+        try:
+            generated_text = ""
+            if args.model == 'MiniCPM':
+                res = model.chat(
+                    image=image,
+                    msgs=msgs,
+                    tokenizer=tokenizer,
+                    sampling=True,
+                    temperature=0.2,
+                    stream=True
+                )
+                for new_text in res:
+                    generated_text += new_text
+            elif args.model == 'GPT':
+                buffered = BytesIO()
+                image.save(buffered, format="JPEG")
+                encoded_image = base64.b64encode(buffered.getvalue()).decode('utf-8')
+                image_base64 = f"data:image/jpeg;base64,{encoded_image}"
+                print("sent requests")
+                response = requests.post(
+                    url="https://openrouter.ai/api/v1/chat/completions",
+                    headers={
+                        "Authorization": f"Bearer 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",
+                    },
+                    data=json.dumps({
+                        "model": "openai/gpt-4o-mini-2024-07-18",  # Optional
+                        "messages": [
+                        {
+                            "role": "user",
+                            "content": [
+                                {
+                                    "type": "text",
+                                    "text": question
+                                },
+                                {
+                                    "type": "image_url",
+                                    "image_url": image_base64
+                                }
+                                ]
+                            }
+                        ]
+                    })
+                )
+                print(response.json())
+                res = response.json()
+                generated_text = res['choices'][0]['message']['content']
+            elif args.model == 'Phi3':
+                model_id = "/home/wangb/zhangrx/models/Phi-3-vision-128k-instruct"
+                model = AutoModelForCausalLM.from_pretrained(model_id, device_map="cuda",
+                                                             trust_remote_code=True,
+                                                             torch_dtype=torch.float16,
+                                                             _attn_implementation='eager')  # use _attn_implementation='eager' to disable flash attention
+                processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
+                msgs_withImage = [
+                    {"role": "user", "content": "<|image_1|>\n " + question}
+                ]
+                prompt = processor.tokenizer.apply_chat_template(msgs_withImage, tokenize=False, add_generation_prompt=True)
+                inputs = processor(prompt, [image], return_tensors="pt").to("cuda:0")
+                generation_args = {
+                    "max_new_tokens": 500,
+                    "temperature": 0.0,
+                    "do_sample": False,
+                }
+                generate_ids = model.generate(**inputs, eos_token_id=processor.tokenizer.eos_token_id,
+                                              **generation_args)
+
+                # remove input tokens
+                generate_ids = generate_ids[:, inputs['input_ids'].shape[1]:]
+                generated_text = (
+                    processor.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False))[0]
+            elif args.model == 'Internvl2':
+                model = "/home/wangb/zhangrx/models/InternVL2-8B"
+                system_prompt = '我是书生·万象，英文名是InternVL，是由上海人工智能实验室及多家合作单位联合开发的多模态大语言模型。'
+                chat_template_config = ChatTemplateConfig('internvl-internlm2')
+                chat_template_config.meta_instruction = system_prompt
+                pipe = pipeline(model, chat_template_config=chat_template_config,
+                                backend_config=TurbomindEngineConfig(session_len=8192))
+                response = pipe((question, image))
+                generated_text = response.text
+            # elif args.model == 'Deepseek':
+            #     model_path = "/home/wangb/zhangrx/models/deepseek-vl-7b-chat"
+            #     vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
+            #     tokenizer = vl_chat_processor.tokenizer
+            #     vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True,
+            #                                                                          torch_dtype=torch.bfloat16)
+            #     vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+            #     conversation = [
+            #         {
+            #             "role": "User",
+            #             "content": question,
+            #             "images": [image]
+            #         },
+            #         {
+            #             "role": "Assistant",
+            #             "content": ""
+            #         }
+            #     ]
+            #
+            #     # load images and prepare for inputs
+            #     pil_images = [image]
+            #     prepare_inputs = vl_chat_processor(
+            #         conversations=conversation,
+            #         images=pil_images,
+            #         force_batchify=True
+            #     ).to(vl_gpt.device)
+            #
+            #     # run image encoder to get the image embeddings
+            #     inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+            #
+            #     # run the model to get the response
+            #     outputs = vl_gpt.language_model.generate(
+            #         inputs_embeds=inputs_embeds,
+            #         attention_mask=prepare_inputs.attention_mask,
+            #         pad_token_id=tokenizer.eos_token_id,
+            #         bos_token_id=tokenizer.bos_token_id,
+            #         eos_token_id=tokenizer.eos_token_id,
+            #         max_new_tokens=512,
+            #         do_sample=False,
+            #         use_cache=True
+            #     )
+            #     generated_text = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+
+            print(generated_text)
+            score = get_score(generated_text)
+            return score, generated_text
+
+        except Exception as e:
+            print(e)
+            continue
+
+
+
+
+def llm_walk(sub_g, start_point, model, tokenizer, images, args):
+    output_words(args, "# Experimental Result")
+    output_words(args, "##  Start Edge Caption")
+
+    now_point = start_point
+
+    id = int(sub_g.nodes[now_point]["image"])
+    image = images[id]
+    output_words(args, f"![no_name](../../data/{city_names[args.city]}/{index}/squeeze_images/{id}.jpg)")
+
+    summary = ask_start_image(model, tokenizer, image, "")
+    output_words(args, summary)
+
+    for iter in range(5):
+
+        output_words(args, f"##  Start Round {iter}")
+
+        neighbors = list(sub_g.neighbors(now_point))
+
+        best_neighbor = -1
+        best_answer = -1
+        best_image = -1
+        for neighbor in neighbors:
+
+            output_words(args, f"###  conside neighbor {neighbor}")
+
+            id = int(sub_g.nodes[neighbor]["image"])
+            now_image = images[id]
+            # ask the answer by the summary
+            answer, reason = ask_middle_image(model, tokenizer, now_image, summary)
+            output_words(args, f"![no_name](../../data/{city_names[args.city]}/{index}/squeeze_images/{id}.jpg)")
+            output_words(args, reason)
+            if answer > best_answer:
+                best_answer = answer
+                best_neighbor = neighbor
+                best_image = now_image
+                output_words(args, "best answer updated!!!!!!!!!!!!!!!!")
+
+        output_words(args, f"###  update summary")
+        summary = ask_summary_image(model, tokenizer, best_image, summary)
+        output_words(args, summary)
+
+        plt.quiver(now_point[0], now_point[1], best_neighbor[0] - now_point[0], best_neighbor[1] - now_point[1]
+                   , angles='xy', scale_units='xy', scale=1, color="r")
+
+        now_point = best_neighbor
+
+    plt.show()
+    # plt.savefig(f"./log/{args.model}/ans.jpg")
+
+
+# os.environ["CUDA_VISIBLE_DEVICES"] = "6"
+# CUDA_VISIBLE_DEVICES=1 python DualVLMWalk.py
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--save_name",
+        type=str,
+        default="Phi3_test",
+        help="save_name",
+    )
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="Phi3",
+        choices=["MiniCPM", "GPT", "Phi3", "Internvl2", "Deepseek"],
+        help="model name",
+    )
+
+    parser.add_argument(
+        "--city",
+        type=int,
+        default=0,
+        choices=[0, 1, 2, 3],  # ["New York City", "San Francisco", "Washington", "Chicago"]
+        help=""
+    )
+
+    parser.add_argument(
+        "--value_path",
+        type=str,
+        default="Carbon",
+        help="type name",
+    )
+
+    parser.add_argument(
+        '--gpu',
+        type=str,
+        default='cuda:0'
+    )
+
+    # print(torch.cuda.is_available())
+    # exit(0)
+    args = parser.parse_args()
+
+    os.makedirs(f"./log/{args.model}", exist_ok=True)
+
+    ava_indexs = load_access_street_view(args.city)
+    model, tokenizer = get_model(args)
+    for index in tqdm(ava_indexs):
+        if int(index) == 14:
+            sub_g, street_views, images = get_graph_and_images_dual(index, args.city, args.value_path)
+            new_g, start_point = shrink_graph(sub_g)
+            llm_walk(new_g, start_point, model, tokenizer, images, args)
+            break
---
+++ vlm_walk_gpt.py
@@ -0,0 +1,270 @@
+# encoding: utf-8
+
+import os
+
+import argparse
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+from tqdm import tqdm, trange
+
+from data.datasets import DownStreamDataset, WalkDataset
+from utils.io_utils import load_access_street_view, load_task_data, calc, init_seed, init_logging, \
+    get_graph_and_images_dual, shrink_graph, parse_json, get_model
+from DualVLMWalk_gpt import ask_start_image, ask_middle_image, ask_summary_image
+import concurrent.futures
+
+
+def output_words(city, index, text, model_dir, model_name="minicpm"):
+    file_name = f"{model_dir}/supervised/{city}/{index}.md"
+    os.makedirs(f"{model_dir}/supervised/{city}/", exist_ok=True)
+
+    # 打开文件，追加内容，然后关闭文件
+    with open(file_name, 'a') as file:
+        file.write("\n")
+        file.write(text)
+        file.write("\n")
+
+
+def output_images(city, index, image, model_dir, model_name="minicpm"):
+    file_name = f"{model_dir}/supervised/{city}/{index}_image.md"
+    os.makedirs(f"{model_dir}/supervised/{city}/", exist_ok=True)
+
+    # 打开文件，追加内容，然后关闭文件
+    with open(file_name, 'a') as file:
+        file.write("\n")
+        file.write(str(image))
+        file.write("\n")
+
+
+def get_image_cnt(city, index, path, model_dir):
+    path = f"{model_dir}/supervised/{city}/{index}_image.md"
+
+    if not os.path.exists(path):
+        return False
+
+    with open(path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    images = [i for i in list(content.split('\n')) if i]
+
+    return len(images) > 10
+
+
+def random_walk(g, start_point, args, index, llm, tokenizer, images, city, model_dir):
+    output_words(city, index, "# Experimental Result", model_dir)
+    output_words(city, index, "##  Start Edge Caption", model_dir)
+
+    id = int(g.nodes[start_point]["image"])
+    image = images[id]
+    output_words(city, index, f"![no_name](../../data/{city}/{index}/squeeze_images/{id}.jpg)", model_dir)
+    output_images(city, index, id, model_dir)
+
+    images_emb = []
+    summary = ask_start_image(llm, tokenizer, image, "", args)
+    output_words(city, index, summary, model_dir)
+
+    for i in trange(9):
+        try:
+            neighbors = list(g.neighbors(start_point))
+            best_neighbor = -1
+            best_answer = -1
+            best_image = -1
+            for neighbor in neighbors:
+                id = int(g.nodes[neighbor]["image"])
+                now_image = images[id]
+                answer, reason = ask_middle_image(llm, tokenizer, now_image, summary, args)
+
+                output_words(city, index,
+                             f"![no_name](../../data/{city}/{index}/squeeze_images/{id}.jpg)", model_dir)
+                output_words(city, index, reason, model_dir)
+
+                if answer > best_answer:
+                    best_answer = answer
+                    best_neighbor = neighbor
+                    best_image = now_image
+                    output_words(city, index, "best answer updated!!!!!!!!!!!!!!!!", model_dir)
+
+            start_point = best_neighbor
+
+            output_words(city, index, f"###  update summary", model_dir)
+            summary = ask_summary_image(llm, tokenizer, best_image, summary, args)
+            output_words(city, index, summary, model_dir)
+
+            output_images(city, index, int(g.nodes[best_neighbor]["image"]), model_dir)
+        except Exception as e:
+            print(e)
+            continue
+    # exit(0)
+    return torch.tensor(images_emb).float()
+
+
+def evaluate(loader, args, llm, tokenizer, city, model_dir, image_dir):
+    for index, y, s in tqdm(loader):
+        index = int(index)
+
+        if get_image_cnt(city, index, "", model_dir):
+            print(f"skip {index}")
+            continue
+        else:
+            if os.path.exists(f"{model_dir}/supervised/{city}/{index}_image.md"):
+                os.remove(f"{model_dir}/supervised/{city}/{index}_image.md")
+                os.remove(f"{model_dir}/supervised/{city}/{index}.md")
+
+        sub_g, street_views, images = get_graph_and_images_dual(index, args.city_size, image_dir)
+
+        new_g, start_point = shrink_graph(sub_g)
+
+        random_walk(new_g, start_point, args, index, llm, tokenizer, images, city, model_dir)
+
+
+city_names = ["New York City", "San Francisco", "Washington", "Chicago"]
+
+
+def main(args):
+    # pip install timm==0.3.2
+    # todo: change timm to 1.0.7
+
+    image_dir, model_dir, save_dir, log_dir, task_dir = parse_json(args)
+
+    init_seed(args.seed)
+
+    idx_dataset = []
+
+    city = args.city_size
+
+    ava_indexs = load_access_street_view(image_dir, city)
+
+    task_data = load_task_data(city, args.target, task_dir)
+
+    for index in tqdm(ava_indexs):
+        sucess_path = f"{image_dir}/{city_names[city]}/{index}/success"
+        if not os.path.exists(sucess_path):
+            continue
+        satellite = np.load(
+            f'{image_dir}/{city_names[city]}/{index}/satellite_embedding_{args.model}.npy')
+        idx_dataset.append([index, task_data[int(index)][-1], satellite])
+
+    # split the dataset into train and test
+    train_size = int(0.7 * len(idx_dataset))
+    val_size = int(0.8 * len(idx_dataset)) - train_size
+    test_size = len(idx_dataset) - train_size - val_size
+
+    train_dataset, val_dataset, test_dataset = \
+        torch.utils.data.random_split(idx_dataset, [train_size, val_size, test_size])
+
+    if len(test_dataset) > 100:
+        test_dataset, _ = torch.utils.data.random_split(test_dataset, [100, len(test_dataset) - 100])
+
+    for index, y, s in test_dataset:
+        index = int(index)
+
+    if args.llm == "FireLLaVA" or args.llm == "Gemini" or args.llm == "Claude":
+        llm, tokenizer = 0 , 0
+    else:
+        llm, tokenizer = get_model(args)
+
+    evaluate(test_dataset, args, llm, tokenizer, city_names[city], model_dir, image_dir)
+
+
+'''
+    CUDA_VISIBLE_DEVICES=2 python vlm_walk.py --city_size 0  --location zrx
+    CUDA_VISIBLE_DEVICES=3 python vlm_walk.py --city_size 1  --location zrx
+    CUDA_VISIBLE_DEVICES=0 python vlm_walk.py --city_size 2  --location wb
+    CUDA_VISIBLE_DEVICES=1 python vlm_walk.py --city_size 3  --location wb
+    
+'''
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--location",
+        type=str,
+        default="wb",
+        help="wb or zrx",
+    )
+
+    parser.add_argument(
+        "--save_name",
+        type=str,
+        default="test",
+        help="save_name",
+    )
+
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="ViT",
+        choices=["MAE", "ResNet", "SimCLR", "CLIP", "ViT"],
+        help="model name",
+    )
+
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=512,
+        help="batch_size",
+    )
+
+    parser.add_argument(
+        "--epoch",
+        type=int,
+        default=100000,
+        help="num epochs",
+    )
+
+    parser.add_argument(
+        "--gpu",
+        type=str,
+        default="cuda:0",
+        help="device",
+    )
+
+    parser.add_argument(
+        "--lr",
+        type=float,
+        default=1e-3,
+        help="lr",
+    )
+
+    # huggingface-cli download --resume-download google/vit-base-patch16-224-in21k --local-dir ./vit-base-patch16-224-in21k
+    parser.add_argument(
+        "--patience",
+        type=int,
+        default=100,
+        help="patience",
+    )
+
+    parser.add_argument(
+        "--city_size",
+        type=int,
+        default=0,
+        help="number of cities",
+    )
+
+    parser.add_argument(
+        "--target",
+        type=int,
+        default=1,
+        help="Carbon or Population",
+    )
+
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=42,
+        help="seed",
+    )
+
+    parser.add_argument(
+        "--llm",
+        type=str,
+        default="FireLLaVA",
+        choices=["minicpm", "FireLLaVA", "Gemini", "Claude"],
+        help="llm",
+    )
+
+    args = parser.parse_args()
+
+    main(args)
